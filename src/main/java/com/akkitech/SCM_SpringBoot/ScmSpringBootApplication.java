1ff9e042cc3ea66fb2f4a74a970d8dbc0bf3d2fe
Akshay Upadhyay <akshayu383@gmail.com>
2024-08-03 22:29:30 +0530

    Configuration completed

---
+++ src/main/java/com/akkitech/SCM_SpringBoot/ScmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.akkitech.SCM_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akkitech/SCM_SpringBoot/ScmSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akkitech.SCM_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
